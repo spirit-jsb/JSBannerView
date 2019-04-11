f22ecb6fead11d6bb7e8570952992f19bb78edd8
Max <sibo_jian_29903549@163.com>
2019-04-11 18:16:31 +0800

    [功能]($\Demo & $\Sources)
    添加 Cocoa Touch Framework 项目主体，对项目文件目录结构调整。
    添加 Single View App 项目主体，对项目文件目录结构调整。
    添加 JSBannerView.xcworkspace 管理上述项目主体。

---
+++ Demo/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSBannerView-Demo
+//
+//  Created by Max on 2019/4/11.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sources/JSBannerView.h
@@ -0,0 +1,19 @@
+//
+//  JSBannerView.h
+//  JSBannerView
+//
+//  Created by Max on 2019/4/11.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSBannerView.
+FOUNDATION_EXPORT double JSBannerViewVersionNumber;
+
+//! Project version string for JSBannerView.
+FOUNDATION_EXPORT const unsigned char JSBannerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSBannerView/PublicHeader.h>
+
+
