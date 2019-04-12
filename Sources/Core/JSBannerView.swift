038b793f3c0e9064b3ec7fe0253cfd30cb76e7ee
Max <sibo_jian_29903549@163.com>
2019-04-12 10:53:04 +0800

    [功能]($JSBannerView)
    初始化 JSBannerView 文件。

---
+++ Sources/Core/JSBannerView.swift
@@ -0,0 +1,13 @@
+//
+//  JSBannerView.swift
+//  JSBannerView
+//
+//  Created by Max on 2019/4/12.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+open class JSBannerView: UIView {
+    
+}
